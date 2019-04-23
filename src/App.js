ca5bc1083f0619bb1b1276fc7343465120ef804c
Anthony DiRusso <ossurid@gmail.com>
2019-04-23 14:48:56 -0400

    creates starter files for greet-react exercises adds lowercase greeting

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GreetLowercase from './GreetLowercase';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <GreetLowercase name="MAX"/>
+
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/GreetLowercase.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function GreetLowercase(props) {
+    return (
+        <h1>
+            Hello {props.name.toLowerCase()} !
+        </h1>
+    )
+}
+
+export default GreetLowercase;
